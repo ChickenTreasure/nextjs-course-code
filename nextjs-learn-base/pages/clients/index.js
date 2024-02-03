40929360b53bd7eaa638d0beaf1214738a0f9cd0
ChickenTreasure <2215090293@qq.com>
2024-02-03 19:06:16 +0800

    feat✨:  引入客户端id

---
+++ nextjs-learn-base/pages/clients/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ClientsPage(){
+    return (
+        <div>
+            <h1>The Clients Page!</h1>
+        </div>
+    )
+}
+
+export default ClientsPage;
